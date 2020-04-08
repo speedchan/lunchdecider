5f2fb3415af37470421d5b4ee91dbb64e7c1ad4f
Lionel <lionelteojiarong@gmail.com>
2020-04-08 12:28:59 +0800

    [backend] Pair programming added Axios cor api calls

--- client/backend/routes/api_routes.js
+++ client/backend/routes/api_routes.js
@@ -20,4 +20,18 @@ module.exports = (app, connection) => {
             res.json({hello: "haloumi"})
         })
     })
+    app.post('/user/register', (req,res) => {
+        
+        let {email, password} = req.body;
+        console.log("reqbody:",req.body)
+        console.log("EMAILPW: ",email, password)
+        connection.query(`INSERT INTO user (email, password) VALUES ('${email}', '${password}')`,(err, results) => {
+            if(err){
+                return res.send(err)
+            } else {
+                return res.send('successfully registered')
+            }
+        })
+    })
+
 }
